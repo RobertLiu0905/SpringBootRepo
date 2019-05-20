d6e3af636b92ce23a1aa74e03ab012c2aaa61e48
Dawn <liu.lin@iwhalecloud.com>
2019-05-20 21:06:09 +0800

    first apply

---
+++ code/demo-parent/demo-bootstrap/src/main/java/com.robert.demo.bootstrap/Bootstrap.java
@@ -0,0 +1,11 @@
+package com.robert.demo.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+
+public class Bootstrap {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Bootstrap.class);
+    }
+}
+
